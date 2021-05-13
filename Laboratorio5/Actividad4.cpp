b37a3877d7dc025f4bba24937f8a2e9d26df5687
Camilo <c.hwarts01@ufromail.com>
2021-05-12 21:51:51 -0400

    Respuesta actividad 4

---
+++ Laboratorio5/Actividad4.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+float radio;
+float area;
+float perimetro;
+float pi = 3.14;
+
+int main (){
+
+	printf("Ingrse el radio de una circunferencia\n ");
+	scanf("%f", &radio);
+
+	area =	pi * radio * radio;
+	perimetro = 2 * pi * radio;
+
+	printf("El valor del area es : %f\n",area);
+	printf("El valor del perimetro es:%f\n", perimetro);
+
+
+	return 0;
+}
