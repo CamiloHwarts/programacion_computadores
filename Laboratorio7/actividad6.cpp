e5f0d8c053443e6d2d88121008cdc222cfd52b8a
Camilo <c.hwarts01@ufromail.com>
2021-06-09 23:54:53 -0400

    Avance Laboratorio7

--- Laboratorio7/actividad6.cpp
+++ Laboratorio7/actividad6.cpp
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main (){
+int num;
+int f0 = 0 , f1 = 1, fb, f3;
 
+int main (){
+  //ingrese un numero 
+  printf("Ingrese un numero:\n");
+  printf("Debe empesar desde el 2 en adelante , ya que los dos primero son 0 y 1\n");
+  scanf("%d", &num);
+
+  printf("0\n");
+  printf("1\n");
+  for (int i = 0 ; i <= num ; i++){
+    //Fibonacii f = fn-1 + fn-2
+    fb = f0 + f1;
+    printf("%d\n", fb);
+    f0 = f1;
+    f1 = fb;
+  }
   return 0;
 }
