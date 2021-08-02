2c470a0ac93e12bd4093a9e6f5de8cc7a6fbce7c
Camilo <c.hwarts01@ufromail.com>
2021-08-02 17:14:20 -0400

    Ayudantias

---
+++ TipsTarea3/Tips.cpp
@@ -0,0 +1,34 @@
+/* #include <stdio.h>
+​
+​
+​
+int main(){
+    // ejemplo con matrices
+    char clientes[3][15];
+​
+    for(int i = 0 ; i < 3 ; i++){
+        printf("Ingrese su nombre:\n");
+        scanf("%s", clientes[i]);
+        printf("Bienvenido %s!!\n", clientes[i]);
+    }
+​
+    printf("%s\n", clientes[0]);
+    printf("%s\n", clientes[1]);
+    printf("%s\n", clientes[2]);
+​
+​
+ // ejemplo con archivos
+    mati
+    1233
+    char nombre[15];
+    char mail[20];
+    char telefono[10];
+    ABRIR ARCHIVO
+    while(HAYA CLIENTES EN EL ARCHIVO){
+        LEO clientes
+        IMRIMO CLIENTE
+    }
+    CERRAR ARCHIVO
+​
+    return 0;
+} */
